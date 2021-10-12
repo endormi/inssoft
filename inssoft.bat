dece927170b50c35709fb4d066582d5c34c49565
endormi <ernosalo14@gmail.com>
2021-10-12 07:48:16 +0300

    Refactor batch script and move repos to separate folders
    * Separate folders meaning my account and the organization

--- inssoft.bat
+++ inssoft.bat
@@ -1,69 +1,25 @@
 @echo off
 
-set "p=autocorrect"
-set "p2=kbsc"
-set "p3=idbf"
-set "p4=find_domain_ip"
-set "p5=automation"
-set "p6=endormi.2077-theme"
-set "p7=webaimcc-cli"
+set "theme=endormi.2077-theme"
 
-IF NOT EXIST %p% (
-  echo "Cloning %p%..."
-  git clone https://github.com/endormi/%p%.git
-  echo "Done!"
-) ELSE (
-  echo %p% Already exists!
-)
-
-IF NOT EXIST %p2% (
-  echo "Cloning %p2%..."
-  git clone https://github.com/endormi/%p2%.git
-  echo "Done!"
-) ELSE (
-  echo %p2% Already exists!
-)
-
-IF NOT EXIST %p3% (
-  echo "Cloning %p3%..."
-  git clone https://github.com/0x020/%p3%.git
-  echo "Done!"
-) ELSE (
-  echo %p3% Already exists!
-)
-
-IF NOT EXIST %p4% (
-  echo "Cloning %p4%..."
-  git clone https://github.com/endormi/%p4%.git
-  echo "Done!"
-) ELSE (
-  echo %p4% Already exists!
-)
-
-IF NOT EXIST %p5% (
-  echo "Cloning %p5%..."
-  git clone https://github.com/endormi/%p5%.git
-  echo "Done!"
-) ELSE (
-  echo %p5% Already exists!
+for %%r in (endormi/autocorrect endormi/kbsc endormi/find_domain_ip endormi/automation endormi/webaimcc-cli 0x020/idbf) do (
+    IF NOT EXIST %%r (
+        echo Cloning %%r...
+        git clone https://github.com/%%r.git %%r
+        echo Done!
+    ) ELSE (
+        echo %%r Already exists!
+    )
 )
 
 IF EXIST "C:\Users\%username%\.vscode" (
   set "ext=extensions.txt"
   code --list-extensions > %ext%
-  find /i "%p6%" %ext% >nul
+  find /i "%theme%" %ext% >nul
   IF ERRORLEVEL 1 (
-    code --install-extension %p6%
+    code --install-extension %theme%
   ) ELSE (
-    echo %p6% Already exists!
+    echo %theme% Already exists!
   )
   DEL %ext%
-)
-
-IF NOT EXIST %p7% (
-  echo "Cloning %p7%..."
-  git clone https://github.com/endormi/%p7%.git
-  echo "Done!"
-) ELSE (
-  echo %p7% Already exists!
 )
