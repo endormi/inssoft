846dda9f034677a04ecafd1904560bd0bdd08af5
endormi <39559256+endormi@users.noreply.github.com>
2021-12-10 22:31:32 +0200

    Set list

--- inssoft.bat
+++ inssoft.bat
@@ -1,8 +1,9 @@
 @echo off
 
+set "list=endormi/autocorrect endormi/kbsc endormi/find_domain_ip endormi/automation endormi/webaimcc-cli 0x020/idbf"
 set "theme=endormi.2077-theme"
 
-for %%r in (endormi/autocorrect endormi/kbsc endormi/find_domain_ip endormi/automation endormi/webaimcc-cli 0x020/idbf) do (
+for %%r in (%list%) do (
     IF NOT EXIST %%r (
         git clone https://github.com/%%r.git %%r
         echo Done!
